c68448eee74c2a9ecc7bcb6c1a78c0b324f73dbc
Bryan Berger <bryan.berger@netsteel.space>
2019-10-05 08:10:23 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'monAgence';
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyCNkhqsSK3Bs_6pvaKIMYtaAklfJBKujWo',
+      authDomain: 'monagence-7bcc4.firebaseapp.com',
+      databaseURL: 'https://monagence-7bcc4.firebaseio.com',
+      projectId: 'monagence-7bcc4',
+      storageBucket: 'monagence-7bcc4.appspot.com',
+      messagingSenderId: '1087915112888',
+      appId: '1:1087915112888:web:1704635fd5c88e7d59774d'
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
+
 }
